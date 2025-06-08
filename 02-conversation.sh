8be86ffcfba7391b8605129aa7609537d94248b7
pavanraju13 <pavanraju4513@gmail.com>
2025-06-08 14:41:59 +0530

    Hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Baby: Hi babu, How are you?
-Babu: I am fine. What about you?
-Baby: I am good. Thanks for asking. I am planning for dinner will you join?
-Babu: Yes, I will come ..Baby where you are planning
-Baby: At my home..Babu
+echo Baby: Hi babu, How are you?
+echo Babu: I am fine. What about you?
+echo Baby: I am good. Thanks for asking. I am planning for dinner will you join?
+echo Babu: Yes, I will come ..Baby where you are planning
+echo Baby: At my home..Babu
 
