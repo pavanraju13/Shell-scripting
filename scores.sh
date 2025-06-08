d5f4956f4a86f9b48b3ee82f5820f232d13d059c
pavanraju13 <pavanraju4513@gmail.com>
2025-06-08 16:17:11 +0530

    Hello world

--- scores.sh
+++ scores.sh
@@ -6,10 +6,10 @@ kholi=15
 sky=45
 pandya=35
 
-total= $(( $Rohit+$Jayswal+$kkholi+$sky+$pandya ))
+total= $(( Rohit + Jayswal + kholi + sky + pandya ))
 
 Extras=20
 
-Toatal_score= $(( $total+$Extras ))
+Toatal_score= $(( total + Extras ))
 
 echo "totalscore of team is : $Total_score"
