3cfecec9905209482688be77fbfa8af87ddff305
pavanraju13 <pavanraju4513@gmail.com>
2025-06-08 14:58:33 +0530

    Hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 
-
+Character1=$Baby
+Character2=$Babu
 echo "
- Baby: Hi babu, How are you?
-Babu: I am fine. What about you?
- Baby: I am good. Thanks for asking. I am planning for dinner will you join?
- Babu: Yes, I will come ..Baby where you are planning
- Baby: At my home..Babu"
+ $Character1: Hi $Character2, How are you?
+ $Character2: I am fine. What about you?
+ $Character1: I am good. Thanks for asking. I am planning for dinner will you join?
+ $Character2: Yes, I will come ..$Character1 where you are planning
+ $Character1: At my home..$Character2"
 
