8b0062d29d14d20447c2445f9df3c3d26d73fc9e
pavanraju13 <pavanraju4513@gmail.com>
2025-06-08 14:40:27 +0530

    Hello world

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Baby: Hi babu, How are you?
+Babu: I am fine. What about you?
+Baby: I am good. Thanks for asking. I am planning for dinner will you join?
+Babu: Yes, I will come ..Baby where you are planning
+Baby: At my home..Babu
+
