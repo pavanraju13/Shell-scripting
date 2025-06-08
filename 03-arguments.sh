360ad204c2ef430aad8130ebc43b2fbe2a3020e6
pavanraju13 <pavanraju4513@gmail.com>
2025-06-08 15:00:08 +0530

    Hello world

---
+++ 03-arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+Character1=$1
+Character2=$2
+echo "
+ $Character1: Hi $Character2, How are you?
+ $Character2: I am fine. What about you?
+ $Character1: I am good. Thanks for asking. I am planning for dinner will you join?
+ $Character2: Yes, I will come ..$Character1 where you are planning
+ $Character1: At my home..$Character2"
