4498c8d500aea490671991a25a5a4f9195221d01
pavanraju13 <pavanraju4513@gmail.com>
2025-06-09 21:56:52 +0530

    Hello world

---
+++ 02-scenario.sh
@@ -0,0 +1,18 @@
+##Write a shell script to simulate a simple login system. Ask the user to enter a username and password. 
+##If the username is 'admin' and the password is 'secret123', print 'Login Successful'. Otherwise, print 'Access Denied'."
+
+
+#!/bin/bash
+
+echo "enter the username:"
+read username
+
+echo "Enter the password:"
+read password
+
+if [ "$username"= "admin" ] && [  "$password" = "secret123" ]
+then
+echo "Login successful"
+else
+echo "Login failed"
+fi
