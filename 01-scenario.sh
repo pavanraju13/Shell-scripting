87eee81a958fedac444006b4c663d60e99e9f404
pavanraju13 <pavanraju4513@gmail.com>
2025-06-09 21:02:26 +0530

    Hello world

--- 01-scenario.sh
+++ 01-scenario.sh
@@ -2,12 +2,14 @@
 ##the employee gets a 20% bonus on the salary. Otherwise, no bonus. Print the total salary accordingly.
 Basic_Salary=$1
 Rating=$2
-Bonus=20%
+
 
 if [ $Rating -ge 4 ]
 then
-echo "Employee gets  $Bonus on the salary"
-Total_salary=$(( $Basic_Salary + $Bonus ))
+echo "Employee gets  $Bonus bonus on the salary"
+Bonus=$(( Basic_Salary * 20 /100 ))
+Salary=$(( Basic_Salary + Bonus ))
+echo "Total salary is $Salary
 else
 echo "No bonus"
 fi
