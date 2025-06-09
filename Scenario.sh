161af8703faf90773b95385f45575724fc20acf0
pavanraju13 <pavanraju4513@gmail.com>
2025-06-09 20:34:45 +0530

    Hello world

--- Scenario.sh
+++ Scenario.sh
@@ -9,25 +9,25 @@ Exam3=$3
 
 if [ $Exam1 -gt 40 ]
 then
-echo "Passed in $Exam1 out of 100
+echo "Passed in $Exam1 out of 100"
 else
-echo "Failed in $Exam1 out of 100
+echo "Failed in $Exam1 out of 100"
 fi
 
 if [ $Exam2 -gt 40 ]
 then
-echo "Passed in $Exam2 out of 100
+echo "Passed in $Exam2 out of 100"
 else
-echo "Failed in $Exam2 out of 100
+echo "Failed in $Exam2 out of 100"
 fi
 
 if [ $Exam3 -gt 40 ]
 then
-echo "Passed in $Exam3 out of 100
+echo "Passed in $Exam3 out of 100"
 else
-echo "Failed in $Exam3 out of 100
+echo "Failed in $Exam3 out of 100"
 fi
 
 echo "The average of three exams score :$Average"
-Average= $( $Exam1 + $Exam2 + $Exam3 )
+Average=$(( Exam1 + Exam2 + Exam3 ))
 
