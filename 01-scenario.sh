eba0d2576fb3116c69ad779899fc56ad59073336
pavanraju13 <pavanraju4513@gmail.com>
2025-06-09 21:03:21 +0530

    Hello world

--- 01-scenario.sh
+++ 01-scenario.sh
@@ -6,9 +6,10 @@ Rating=$2
 
 if [ $Rating -ge 4 ]
 then
-echo "Employee gets  $Bonus bonus on the salary"
+
 Bonus=$(( Basic_Salary * 20 /100 ))
 Salary=$(( Basic_Salary + Bonus ))
+echo "Employee gets  $Bonus bonus on the salary"
 echo "Total salary is $Salary
 else
 echo "No bonus"
