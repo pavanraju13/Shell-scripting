f653ab900f389419b0687c9a0b2c5715b2cf8a22
pavanraju13 <pavanraju4513@gmail.com>
2025-06-09 21:17:01 +0530

    Hello world

--- 01-scenario.sh
+++ 01-scenario.sh
@@ -1,11 +1,10 @@
 ##Write a shell script that reads an employee's basic salary and performance rating. If the rating is greater than or equal to 4 (on a scale of 5), 
 ##the employee gets a 20% bonus on the salary. Otherwise, no bonus. Print the total salary accordingly.
  echo "Enter Basic_Salary:"
- read $Basic_Salary
+ read Basic_Salary
 
 echo "Enter the Rating:"
-read $Rating
-
+read Rating
 
 if [ $Rating -ge 4 ]
 then
