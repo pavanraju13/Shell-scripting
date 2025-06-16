8f8a99a00852a06105a0dc8cdd6008c414c46406
pavanraju13 <pavanraju4513@gmail.com>
2025-06-16 10:07:21 +0530

    Install packages

--- 10-functions.sh
+++ 10-functions.sh
@@ -3,34 +3,37 @@
 #This is function for the repated code
 
 USER_ID=$( id -u )
-package=$1
+package=python3
 
 
 
 Check_Install() {
-    dnf list installed $1
 
-if [ $? -ne 0 ]
-then
-echo "$1 is not installed.. Moving further to installation of $1"
-      dnf install $1 -y
-     if [ $? -ne 0 ]
+     if [ $1 -ne 0 ]
      then 
-     echo "$1 is installed successfully"
+     echo "python3 is installed successfully"
      else
-     echo "$1 is not installed successfully".
+     echo "python3 is not installed successfully".
      exit 1
      fi
-else
-echo -e "$1 is already installed.. \nThank you"
-fi 
 }
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+      echo "python3 is not installed.. Moving further to installation of python3"
+      dnf install python3 -y
+      Check_Install $? "python3"
+    else
+       echo -e "python3 is already installed.. \nThank you"
+fi 
+
 
 
 if [ $USER_ID -ne 0 ]
 then 
 echo "Please run the command with root privileges"
-exit $1
+exit 1
 else
 echo "you are root user"
 fi
