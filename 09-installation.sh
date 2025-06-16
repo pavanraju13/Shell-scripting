09d6765f35751d747ad71772f67ada75e6609e4e
pavanraju13 <pavanraju4513@gmail.com>
2025-06-16 09:16:45 +0530

    Package installation

--- 09-installation.sh
+++ 09-installation.sh
@@ -3,6 +3,7 @@
 #This is to install the packages
 
 USER_ID=$( id -u )
+Package1=$1
 
 if [ $USER_ID -ne 0 ]
 then 
@@ -12,5 +13,18 @@ else
 echo "you are root user"
 fi
 
+dnf list installed $1
 
-dnf install mysql -y
+if [ $? -ne 0 ]
+then
+echo "$1 is not installed.. Moving further to installation of mysql"
+      dnf install $1 -y
+     if [ $? -ne 0 ]
+     then 
+     echo "$1 is installed successfully"
+     else
+     echo "$1 is not installed successfully".
+     fi
+else
+echo -e "$1 is already installed".. /n Thank you
+fi
