7479186f35cbace2f14549184f70553047f9c47b
pavanraju13 <pavanraju4513@gmail.com>
2025-06-16 18:14:49 +0530

    Install packages

--- 13-colors.sh
+++ 13-colors.sh
@@ -13,6 +13,9 @@ TIME_STAMP=$(date +%Y-%m-%d_%H-%M-%S)
 USER_ID=$(id -u)
 LOG_FOLDER="/var/log/shell-scripting-logs"
 LOG_FILE="$LOG_FOLDER/${SCRIPT_NAME}.log"
+Package=( "mysql" "python3" "git" "httpd")
+
+echo "script starting at $TIME_STAMP"
 
 if [ $USER_ID -eq 0 ]
 then
@@ -20,3 +23,25 @@ echo -e ${G} you are root user. ${N} | tee -a $LOG_FILE
 else 
 echo -e ${R} You dont have root privileges. ${N} | tee -a $LOG_FILE
 fi
+
+Check_Install() {
+    if [ $1 -eq 0 ]
+    then
+    echo -e $G $Package is installed successfully $N
+    else
+    echo  -e $R $Package is not installed successfully $N
+    fi
+}
+
+dnf list installed $Package &>> $LOG_FILE
+
+if [ $? -eq 0 ]
+then
+echo -e $G $Package is already installed $N
+else
+echo -e $Y $Package is not installed..Going to install it $N 
+      dnf install -y $Package &>> $LOG_FILE
+      Check_Install $? 
+fi
+
+
